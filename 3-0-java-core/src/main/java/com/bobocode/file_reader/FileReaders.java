53c1be220675dc564dc46a8619cabc6539f2fb1a
Serhii <mathodcoast@gmail.com>
2021-01-05 16:55:54 +0200

    GP-28 Add Migrate File Reader to main

---
+++ 3-0-java-core/src/main/java/com/bobocode/file_reader/FileReaders.java
@@ -0,0 +1,19 @@
+package com.bobocode.file_reader;
+
+import com.bobocode.util.ExerciseNotCompletedException;
+
+/**
+ * {@link FileReaders} provides an API that allow to read whole file into a {@link String} by file name.
+ */
+public class FileReaders {
+
+    /**
+     * Returns a {@link String} that contains whole text from the file specified by name.
+     *
+     * @param fileName a name of a text file
+     * @return string that holds whole file content
+     */
+    public static String readWholeFile(String fileName) {
+        throw new ExerciseNotCompletedException(); //todo
+    }
+}
---
+++ 3-0-java-core/src/test/java/com/bobocode/file_reader/FileReadersTest.java
@@ -0,0 +1,36 @@
+package com.bobocode.file_reader;
+
+import com.bobocode.file_reader.FileReaders;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class FileReadersTest {
+
+    @Test
+    void testReadWholeFileOnEmptyFile() {
+        String fileContent = FileReaders.readWholeFile("empty.txt");
+
+        assertEquals("", fileContent);
+
+    }
+
+    @Test
+    void testReadWholeFileOnFileWithEmptyLines() {
+        String fileContent = FileReaders.readWholeFile("lines.txt");
+
+        assertEquals("Hey!\n" +
+                "\n" +
+                "What's up?\n" +
+                "\n" +
+                "Hi!", fileContent);
+    }
+
+    @Test
+    void testReadWholeFile() {
+        String fileContent = FileReaders.readWholeFile("simple.txt");
+
+        assertEquals("Hello!\n" + "It's a test file.", fileContent);
+    }
+}
