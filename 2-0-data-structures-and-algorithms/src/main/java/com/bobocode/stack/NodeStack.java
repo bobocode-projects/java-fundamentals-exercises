c853804570d23810b3abcd8e6dde1d993fbc9c1a
Serhii <mathodcoast@gmail.com>
2021-01-06 13:06:16 +0200

    GP-32 Add javadoc for NodeStack

--- 2-0-data-structures-and-algorithms/src/main/java/com/bobocode/stack/NodeStack.java
+++ 2-0-data-structures-and-algorithms/src/main/java/com/bobocode/stack/NodeStack.java
@@ -1,29 +1,65 @@
 package com.bobocode.stack;
 
+import com.bobocode.stack.exception.EmptyStackException;
 import com.bobocode.util.ExerciseNotCompletedException;
 
+/**
+ * {@link NodeStack} represents a last-in-first-out (LIFO) stack of objects that is based on singly linked generic nodes.
+ * A node is implemented as inner static class {@link Node<T>}.
+ *
+ * @param <T> generic type parameter
+ */
 public class NodeStack<T> implements Stack<T> {
 
-
+    /**
+     * This method creates a stack of provided elements
+     *
+     * @param elements elements to add
+     * @param <T>      generic type
+     * @return a new stack of elements that were passed as method parameters
+     */
     public static <T> NodeStack<T> of(T... elements) {
         throw new ExerciseNotCompletedException(); // todo: implement this method
     }
 
+    /**
+     * The method pushes an element onto the top of this stack. This has exactly the same effect as:
+     * addElement(item)
+     *
+     * @param element elements to add
+     */
     @Override
     public void push(T element) {
         throw new ExerciseNotCompletedException(); // todo: implement this method
     }
 
+    /**
+     * This method removes the object at the top of this stack
+     * and returns that object as the value of this function.
+     *
+     * @return The object at the top of this stack
+     * @throws EmptyStackException - if this stack is empty
+     */
     @Override
     public T pop() {
         throw new ExerciseNotCompletedException(); // todo: implement this method
     }
 
+    /**
+     * Returns the number of elements in the stack
+     *
+     * @return number of elements
+     */
     @Override
     public int size() {
         throw new ExerciseNotCompletedException(); // todo: implement this method
     }
 
+    /**
+     * Checks if a stack is empty
+     *
+     * @return {@code true} if a stack is empty, {@code false} otherwise
+     */
     @Override
     public boolean isEmpty() {
         throw new ExerciseNotCompletedException(); // todo: implement this method;
