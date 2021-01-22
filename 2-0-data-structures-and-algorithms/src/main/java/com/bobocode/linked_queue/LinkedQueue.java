8b88fdce040ba7663d03415743bbb6f812c287b6
Ivan <erneman@rambler.ru>
2021-01-22 12:56:42 +0200

    GP-27: migrate main classes and tests (linked_queue)

---
+++ 2-0-data-structures-and-algorithms/src/main/java/com/bobocode/linked_queue/LinkedQueue.java
@@ -0,0 +1,49 @@
+package com.bobocode.linked_queue;
+
+import com.bobocode.util.ExerciseNotCompletedException;
+
+/**
+ * {@link LinkedQueue} implements FIFO {@link Queue}, using singly linked nodes. Nodes are stores in instances of nested
+ * class Node. In order to perform operations {@link LinkedQueue#add(Object)} and {@link LinkedQueue#poll()}
+ * in a constant time, it keeps to references to the head and tail of the queue.
+ *
+ * @param <T> a generic parameter
+ */
+public class LinkedQueue<T> implements Queue<T> {
+
+    /**
+     * Adds an element to the end of the queue.
+     *
+     * @param element the element to add
+     */
+    public void add(T element) {
+        throw new ExerciseNotCompletedException(); // todo: implement this method
+    }
+
+    /**
+     * Retrieves and removes queue head.
+     *
+     * @return an element that was retrieved from the head or null if queue is empty
+     */
+    public T poll() {
+        throw new ExerciseNotCompletedException(); // todo: implement this method
+    }
+
+    /**
+     * Returns a size of the queue.
+     *
+     * @return an integer value that is a size of queue
+     */
+    public int size() {
+        throw new ExerciseNotCompletedException(); // todo: implement this method
+    }
+
+    /**
+     * Checks if the queue is empty.
+     *
+     * @return {@code true} if the queue is empty, returns {@code false} if it's not
+     */
+    public boolean isEmpty() {
+        throw new ExerciseNotCompletedException(); // todo: implement this method
+    }
+}
