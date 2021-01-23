9fa96170d072a106e53dec9a2f80f8d64d93d779
Ivan <erneman@rambler.ru>
2021-01-23 09:40:06 +0200

    GP-27 complete LinkedQueue

--- 2-0-data-structures-and-algorithms/src/main/java/com/bobocode/linked_queue/LinkedQueue.java
+++ 2-0-data-structures-and-algorithms/src/main/java/com/bobocode/linked_queue/LinkedQueue.java
@@ -1,7 +1,5 @@
 package com.bobocode.linked_queue;
 
-import com.bobocode.util.ExerciseNotCompletedException;
-
 /**
  * {@link LinkedQueue} implements FIFO {@link Queue}, using singly linked nodes. Nodes are stores in instances of nested
  * class Node. In order to perform operations {@link LinkedQueue#add(Object)} and {@link LinkedQueue#poll()}
@@ -10,6 +8,22 @@ import com.bobocode.util.ExerciseNotCompletedException;
  * @param <T> a generic parameter
  */
 public class LinkedQueue<T> implements Queue<T> {
+    static final class Node<T> {
+        private T element;
+        private Node<T> next;
+
+        static <T> Node<T> valueOf(T element) {
+            return new Node<>(element);
+        }
+
+        private Node(T element) {
+            this.element = element;
+        }
+    }
+
+    private Node<T> head;
+    private Node<T> tail;
+    private int size;
 
     /**
      * Adds an element to the end of the queue.
@@ -17,7 +31,14 @@ public class LinkedQueue<T> implements Queue<T> {
      * @param element the element to add
      */
     public void add(T element) {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
+        Node<T> newNode = Node.valueOf(element);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
     }
 
     /**
@@ -26,7 +47,17 @@ public class LinkedQueue<T> implements Queue<T> {
      * @return an element that was retrieved from the head or null if queue is empty
      */
     public T poll() {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
+        if (head != null) {
+            T element = head.element;
+            head = head.next;
+            if (head == null) {
+                tail = null;
+            }
+            size--;
+            return element;
+        } else {
+            return null;
+        }
     }
 
     /**
@@ -35,7 +66,7 @@ public class LinkedQueue<T> implements Queue<T> {
      * @return an integer value that is a size of queue
      */
     public int size() {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
+        return size;
     }
 
     /**
@@ -44,6 +75,6 @@ public class LinkedQueue<T> implements Queue<T> {
      * @return {@code true} if the queue is empty, returns {@code false} if it's not
      */
     public boolean isEmpty() {
-        throw new ExerciseNotCompletedException(); // todo: implement this method
+        return head == null;
     }
 }
