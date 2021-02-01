ed2e1de61479a06be6d35fa796d45c5ae35a9eb0
Taras Boychuk <tarass.boychuk@gmail.com>
2021-02-01 10:13:11 +0200

    GP-39 fix RecursiveBinarySearchTree.java

--- 6-0-test-driven-development/6-1-3-binary-search-tree/src/main/java/com/bobocode/tdd/RecursiveBinarySearchTree.java
+++ 6-0-test-driven-development/6-1-3-binary-search-tree/src/main/java/com/bobocode/tdd/RecursiveBinarySearchTree.java
@@ -1,37 +1,127 @@
 package com.bobocode.tdd;
 
-import com.bobocode.util.ExerciseNotCompletedException;
-
+import java.util.Objects;
 import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 public class RecursiveBinarySearchTree<T extends Comparable> implements BinarySearchTree<T> {
+    private static class Node<T> {
+        T element;
+        Node<T> left;
+        Node<T> right;
+
+        private Node(T element) {
+            this.element = element;
+        }
+
+        public static <T> Node valueOf(T element) {
+            return new Node(element);
+        }
+    }
+
+    private Node<T> root;
+    private int size = 0;
 
     public static <T extends Comparable> RecursiveBinarySearchTree<T> of(T... elements) {
-        throw new ExerciseNotCompletedException();
+        RecursiveBinarySearchTree<T> bst = new RecursiveBinarySearchTree<>();
+        Stream.of(elements).forEach(bst::insert);
+        return bst;
     }
 
     @Override
     public boolean insert(T element) {
-        throw new ExerciseNotCompletedException();
+        Objects.requireNonNull(element);
+        boolean isInserted = insertElement(element);
+        if (isInserted) {
+            size++;
+        }
+        return isInserted;
     }
+
+    boolean insertElement(T element) {
+        if (root == null) {
+            root = Node.valueOf(element);
+            return true;
+        } else {
+            return insertIntoSubTree(root, element);
+        }
+    }
+
+    private boolean insertIntoSubTree(Node<T> subTreeRoot, T element) {
+        if (subTreeRoot.element.compareTo(element) > 0) {
+            return insertIntoLeftSubtree(subTreeRoot, element);
+        } else if (subTreeRoot.element.compareTo(element) < 0) {
+            return insertIntoRightSubtree(subTreeRoot, element);
+        } else {
+            return false;
+        }
+    }
+
+    private boolean insertIntoLeftSubtree(Node<T> node, T element) {
+        if (node.left != null) {
+            return insertIntoSubTree(node.left, element);
+        } else {
+            node.left = Node.valueOf(element);
+            return true;
+        }
+    }
+
+    private boolean insertIntoRightSubtree(Node<T> node, T element) {
+        if (node.right != null) {
+            return insertIntoSubTree(node.right, element);
+        } else {
+            node.right = Node.valueOf(element);
+            return true;
+        }
+    }
+
 
     @Override
     public boolean contains(T element) {
-        throw new ExerciseNotCompletedException();
+        Objects.requireNonNull(element);
+        return findChildNodeByElement(root, element) != null;
+    }
+
+    private Node<T> findChildNodeByElement(Node<T> node, T element) {
+        if (node == null) {
+            return null;
+        } else if (node.element.compareTo(element) > 0) {
+            return findChildNodeByElement(node.left, element);
+        } else if (node.element.compareTo(element) < 0) {
+            return findChildNodeByElement(node.right, element);
+        } else {
+            return node;
+        }
     }
 
     @Override
     public int size() {
-        throw new ExerciseNotCompletedException();
+        return size;
     }
 
     @Override
     public int depth() {
-        throw new ExerciseNotCompletedException();
+        return root != null ? depth(root) - 1 : 0;
+    }
+
+    private int depth(Node<T> node) {
+        if (node == null) {
+            return 0;
+        } else {
+            return 1 + Math.max(depth(node.left), depth(node.right));
+        }
     }
 
     @Override
     public void inOrderTraversal(Consumer<T> consumer) {
-        throw new ExerciseNotCompletedException();
+        inOrderTraversal(root, consumer);
+    }
+
+    private void inOrderTraversal(Node<T> node, Consumer<T> consumer) {
+        if (node != null) {
+            inOrderTraversal(node.left, consumer);
+            consumer.accept(node.element);
+            inOrderTraversal(node.right, consumer);
+        }
     }
 }
