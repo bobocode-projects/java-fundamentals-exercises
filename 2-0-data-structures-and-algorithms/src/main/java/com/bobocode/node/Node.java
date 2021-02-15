5e606d52a51c95eb89ea6cc0ef6c027b4c312f66
Serhii <mathodcoast@gmail.com>
2021-02-15 18:12:12 +0200

    Fix the grammatical mistake

--- 2-0-data-structures-and-algorithms/src/main/java/com/bobocode/node/Node.java
+++ 2-0-data-structures-and-algorithms/src/main/java/com/bobocode/node/Node.java
@@ -2,7 +2,7 @@ package com.bobocode.node;
 
 /**
  * Class {@link Node} is a very simple data structure that consists of an element itself and the reference to the next
- * node. A element can have any value since it's a generic. A reference to the next node allows to link {@link Node}
+ * node. An element can have any value since it's a generic. A reference to the next node allows to link {@link Node}
  * objects and build more comprehensive data structures on top of those liked nodes.
  *
  * @param <T> a generic type T
