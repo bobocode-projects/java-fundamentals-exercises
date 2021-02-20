f3d744a24ae9d078c433f247697fbef383003859
fugrusha <andrew.golowko@gmail.com>
2021-02-20 18:56:24 +0200

    RecursiveBinarySearchTree implementation

--- 2-0-data-structures-and-algorithms/src/main/java/com/bobocode/bst/RecursiveBinarySearchTree.java
+++ 2-0-data-structures-and-algorithms/src/main/java/com/bobocode/bst/RecursiveBinarySearchTree.java
@@ -1,37 +1,117 @@
 package com.bobocode.bst;
 
-import com.bobocode.util.ExerciseNotCompletedException;
-
+import java.util.Objects;
 import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 public class RecursiveBinarySearchTree<T extends Comparable<T>> implements BinarySearchTree<T> {
 
+    private static class Node<T> {
+        T element;
+        Node<T> left;
+        Node<T> right;
+
+        public Node(T element) {
+            this.element = element;
+        }
+    }
+
+    private Node<T> root;
+    private int size;
+
     public static <T extends Comparable<T>> RecursiveBinarySearchTree<T> of(T... elements) {
-        throw new ExerciseNotCompletedException();
+        RecursiveBinarySearchTree<T> tree = new RecursiveBinarySearchTree<>();
+        Stream.of(elements).forEach(tree::insert);
+        return tree;
     }
 
     @Override
     public boolean insert(T element) {
-        throw new ExerciseNotCompletedException();
+        Objects.requireNonNull(element);
+        boolean isInserted = insertElement(element);
+
+        if (isInserted) {
+            size++;
+        }
+
+        return isInserted;
+    }
+
+    private boolean insertElement(T element) {
+        if (root == null) {
+            root = new Node<>(element);
+            return true;
+        } else {
+            return insertToSubTree(root, element);
+        }
+    }
+
+    private boolean insertToSubTree(Node<T> current, T element) {
+        if (element.compareTo(current.element) < 0) { // when less that current go to left
+            if (current.left == null) {
+                current.left = new Node<>(element);
+                return true;
+            } else {
+                return insertToSubTree(current.left, element);
+            }
+        } else if (element.compareTo(current.element) > 0) { // when grater then current go to right
+            if (current.right == null) {
+                current.right = new Node<>(element);
+                return true;
+            } else {
+                return insertToSubTree(current.right, element);
+            }
+        } else { // when elements are equal
+            return false;
+        }
     }
 
     @Override
     public boolean contains(T element) {
-        throw new ExerciseNotCompletedException();
+        Objects.requireNonNull(element);
+        return contains(root, element);
+    }
+
+    private boolean contains(Node<T> current, T element) {
+        if (current == null) {
+            return false;
+        } else if (element.compareTo(current.element) > 0) {
+            return contains(current.right, element);
+        } else if (element.compareTo(current.element) < 0) {
+            return contains(current.left, element);
+        } else {
+            return true;
+        }
     }
 
     @Override
     public int size() {
-        throw new ExerciseNotCompletedException();
+        return size;
     }
 
     @Override
     public int depth() {
-        throw new ExerciseNotCompletedException();
+        return root != null ? depth(root) - 1 : 0;
+    }
+
+    private int depth(Node<T> current) {
+        if (current == null) {
+            return 0;
+        } else {
+            return 1 + Math.max(depth(current.left), depth(current.right));
+        }
     }
 
     @Override
     public void inOrderTraversal(Consumer<T> consumer) {
-        throw new ExerciseNotCompletedException();
+        inOrderTraversal(root, consumer);
+    }
+
+    private void inOrderTraversal(Node<T> current, Consumer<T> consumer) {
+        if (current != null) {
+            inOrderTraversal(current.left, consumer);
+            consumer.accept(current.element);
+            inOrderTraversal(current.right, consumer);
+        }
     }
 }
