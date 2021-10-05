c1a0c8186a0319ed42f0a79bb44efe225acb92cc
Taras Boychuk <tarass.boychuk@gmail.com>
2021-10-05 13:34:28 +0300

    GP-39 fun-prime-numbers * refactor main to introduce 3 more methods and corresponding tests

--- 5-0-functional-programming/5-4-1-fun-prime-numbers/src/main/java/com/bobocode/fp/PrimeNumbers.java
+++ 5-0-functional-programming/5-4-1-fun-prime-numbers/src/main/java/com/bobocode/fp/PrimeNumbers.java
@@ -3,7 +3,9 @@ package com.bobocode.fp;
 import com.bobocode.util.ExerciseNotCompletedException;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.IntConsumer;
+import java.util.stream.IntStream;
 
 /**
  * {@link PrimeNumbers} provides an API to work with prime numbers. The implementation is based on the
@@ -16,6 +18,26 @@ public class PrimeNumbers {
     }
 
     /**
+     * Generates an infinite int stream of prime numbers.
+     * The stream values are 2, 3, 5,... and so on.
+     *
+     * @return an infinite int stream of prime numbers
+     */
+    public static IntStream stream() {
+        throw new ExerciseNotCompletedException(); // todo: create an infinite stream of ints, then filter prime numbs
+    }
+
+    /**
+     * Generates an int stream of a certain amount of prime numbers.
+     * It is based on the {@link PrimeNumbers#stream()} but specifies the exact size of the stream.
+     *
+     * @return an int stream of prime numbers with a specified size
+     */
+    public static IntStream stream(int size) {
+        throw new ExerciseNotCompletedException(); // todo: use the prev to generate a stream method but limit its size
+    }
+
+    /**
      * Calculates the sum on first n prime numbers.
      * E.g. if n = 5, the result should be 2 + 3 + 5 + 7 + 11 = 28
      *
@@ -23,7 +45,7 @@ public class PrimeNumbers {
      * @return the sum of n prime numbers
      */
     public static int sum(int n) {
-        throw new ExerciseNotCompletedException(); // todo: create an infinite stream of ints, then filter prime numbs
+        throw new ExerciseNotCompletedException(); // todo: use prev method and calculate the sum
 
     }
 
@@ -32,8 +54,8 @@ public class PrimeNumbers {
      *
      * @return a list of collected prime numbers
      */
-    public static List<Integer> collect(int n) {
-        throw new ExerciseNotCompletedException(); // todo: reuse the logic of prime numbers stream and collect them
+    public static List<Integer> list(int n) {
+        throw new ExerciseNotCompletedException(); // todo: collect prime numbers into the list
     }
 
     /**
@@ -43,6 +65,20 @@ public class PrimeNumbers {
      * @param consumer a logic that should be applied to the found prime number
      */
     public static void processByIndex(int idx, IntConsumer consumer) {
-        throw new ExerciseNotCompletedException(); // todo: reuse the logic of prime numbers stream then process the last one
+        throw new ExerciseNotCompletedException(); // todo: find an element in the stream by index and process it
+    }
+
+    /**
+     * Creates a list of n prime numbers and returns a map where all of those prime numbers are groped. The key represents
+     * an amount of digits and the value is a corresponding list of all prime numbers.
+     * <p>
+     * So if you will call this method for with argument 20, you will receive the following map:
+     * {1=[2, 3, 5, 7], 2=[11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]}
+     *
+     * @param n – the amount of prime numbers
+     * @return a map with prime number grouped by the amount of digits
+     */
+    public static Map<Integer, List<Integer>> groupByAmountOfDigits(int n) {
+        throw new ExerciseNotCompletedException(); // todo: group n prime numbers by the amount of digits
     }
 }
