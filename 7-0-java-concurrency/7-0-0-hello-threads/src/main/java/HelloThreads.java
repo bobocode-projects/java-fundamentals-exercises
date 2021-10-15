4fc6d361c8060f406ca3491440788233be854bd0
tkuzub <taras.kuzub.ua@gmail.com>
2021-10-15 15:57:22 +0300

    GP-106 fix after review

--- 7-0-java-concurrency/7-0-0-hello-threads/src/main/java/HelloThreads.java
+++ 7-0-java-concurrency/7-0-0-hello-threads/src/main/java/HelloThreads.java
@@ -3,68 +3,82 @@ import lombok.SneakyThrows;
 
 import java.util.List;
 
+import static java.lang.Thread.*;
+
 public class HelloThreads {
     /**
-     * Receives a {@link Runnable} parameter, and returns a {@link Thread}.
+     * Receives a {@link Runnable} parameter that holds the logic and creates a {@link Thread} instance based on it.
+     *
      * @param runnable the code you want to run in new thread
      * @return a new thread
      */
-    public static Thread runningThread(Runnable runnable) {
+    public static Thread createThread(Runnable runnable) {
         throw new ExerciseNotCompletedException();
     }
 
     /**
-     * Receive a {@link Thread} parameter and start it
-     * @param thread the code you want to start thread
+     * Receives a {@link Thread} parameter created based on {@link Runnable},
+     *
+     * @param thread the code you want to run
      */
-    public static void runningThreadViaStart(Thread thread) {
+    public static void startThread(Thread thread) {
         throw new ExerciseNotCompletedException();
     }
 
     /**
-     * Receives a {@link Thread} parameter, and return a {@link String}
-     * @param thread the code you want start thread and return tread name
+     * Receives a {@link Thread} parameter created based on {@link Runnable},
+     * and return {@link String} that contains the name this thread
+     *
+     * @param thread the code you want run
      * @return the name thread
      */
-    public static String runningThreadGetNameThread(Thread thread) {
+    public static String getThreadName(Thread thread) {
         throw new ExerciseNotCompletedException();
     }
 
     /**
-     * Receives a {@link Thread} parameter, and return a {@link Thread.State}
-     * @param thread the code you want start thread and return state
+     * Receives a {@link Thread} parameter created based on {@link Runnable},
+     * and return a {@link State} that contains the state this thread
+     *
+     * @param thread the code you want run
      * @return the thread state
      */
-    public static Thread.State runningThreadGetStateThread(Thread thread) {
+    public static State getThreadState(Thread thread) {
         throw new ExerciseNotCompletedException();
     }
 
     /**
-     * Receives a {@link Runnable} parameter, create new {@link Thread}, start and return this {@link Thread}
-     * @param runnable the code you want to run in new thread and start
-     * @return this thread
+     * Receives a {@link Runnable} parameter that holds some logic, create new {@link Thread} instance based on it,
+     * start and return this instance
+     *
+     * @param runnable the code you want to run in new thread
+     * @return the instance this thread
      */
-    public static Thread getSomeLogicRunningTreadAndReturnThisThread(Runnable runnable) {
+    public static Thread runInNewThread(Runnable runnable) {
         throw new ExerciseNotCompletedException();
     }
 
     /**
-     * Receives a {@link Runnable} parameter, create new {@link Thread}, start and wait when it has completed
-     * @param runnable - the code you want to run in new thread and start
+     * Receives a {@link Runnable} parameter that holds the logic and create new {@link Thread} instance bases on it,
+     * start and wait when it has completed
+     *
+     * @param runnable - the code you want to run in new threads
      */
     @SneakyThrows
-    public static void runningThreadAndWhenJoinCompleted(Runnable runnable) {
+    public static void runInNewThreadAndWaitForExecution(Runnable runnable) {
         throw new ExerciseNotCompletedException();
     }
 
     /**
-     * Receives a {@link Runnable} parameter, create three new {@link Thread} start it,
+     * Receives a {@link Runnable} parameter that holds the logic and creates three {@link Thread} instance based on it
      * and return list of threads that are in progress
-     * @param runnable the code you want to run in new thread and start
-     * @return list these threads
+     *
+     * @param runnable the code you want to run in new thread
+     * @return the list of running threads
      */
     @SneakyThrows
-    public static List<Thread> runningMultipleThreadsWithOneTask(Runnable runnable) {
+    public static List<Thread> runInMultipleThreads(Runnable runnable) {
         throw new ExerciseNotCompletedException();
     }
 }
+
--- 7-0-java-concurrency/7-0-0-hello-threads/src/test/java/HelloThreadsTest.java
+++ 7-0-java-concurrency/7-0-0-hello-threads/src/test/java/HelloThreadsTest.java
@@ -14,93 +14,93 @@ class HelloThreadsTest {
     private Queue<Integer> concurrentLinkedQueue;
 
     @BeforeEach
-    void prepare() {
+    void setup() {
         concurrentLinkedQueue = new ConcurrentLinkedQueue<>();
     }
 
     @SneakyThrows
     @Test
     @Order(1)
-    void runningThread() {
-        Thread runningThread = HelloThreads.runningThread(() -> concurrentLinkedQueue.add(5));
+    void createThread() {
+        Thread runningThread = HelloThreads.createThread(() -> concurrentLinkedQueue.add(5));
         assertEquals(0, concurrentLinkedQueue.size());
 
         runningThread.start();
         runningThread.join();
 
-        checkContentQueue();
+        verifyContentQueue();
     }
 
     @SneakyThrows
     @Test
     @Order(2)
-    void runningThreadViaStart() {
+    void startThread() {
         var thread = new Thread(() -> concurrentLinkedQueue.add(5));
-        HelloThreads.runningThreadViaStart(thread);
+        HelloThreads.startThread(thread);
         thread.join();
 
-        checkContentQueue();
+        verifyContentQueue();
     }
 
     @SneakyThrows
     @Test
     @Order(3)
-    void runningThreadGetNameThread() {
+    void getThreadName() {
         var thread = new Thread(() -> concurrentLinkedQueue.add(5), "name");
-        var threadName = HelloThreads.runningThreadGetNameThread(thread);
+        var threadName = HelloThreads.getThreadName(thread);
         thread.join();
         assertEquals("name", threadName);
 
-        checkContentQueue();
+        verifyContentQueue();
     }
 
     @SneakyThrows
     @Test
     @Order(3)
-    void runningThreadGetStateThread() {
+    void getThreadState() {
         var thread = new Thread(() -> concurrentLinkedQueue.add(5));
-        var state = HelloThreads.runningThreadGetStateThread(thread);
+        var state = HelloThreads.getThreadState(thread);
         thread.join();
         assertSame(state, Thread.State.RUNNABLE);
 
-        checkContentQueue();
+        verifyContentQueue();
     }
 
     @SneakyThrows
     @Test
     @Order(4)
-    void getSomeLogicRunningTreadAndReturnThisThread()  {
-        var thread = HelloThreads.getSomeLogicRunningTreadAndReturnThisThread(new Thread(() -> {
+    void runInNewThread() {
+        var thread = HelloThreads.runInNewThread(new Thread(() -> {
             assertEquals(0, concurrentLinkedQueue.size());
             concurrentLinkedQueue.add(5);
         }));
         thread.join();
 
-        checkContentQueue();
+        verifyContentQueue();
     }
 
     @SneakyThrows
     @Test
     @Order(5)
-    void runningThreadAndWhenJoinCompleted()  {
-        HelloThreads.runningThreadAndWhenJoinCompleted(new Thread(() -> {
+    void runInNewThreadAndWaitForExecution() {
+        HelloThreads.runInNewThreadAndWaitForExecution(new Thread(() -> {
             assertEquals(0, concurrentLinkedQueue.size());
             concurrentLinkedQueue.add(5);
         }));
 
-        checkContentQueue();
+        verifyContentQueue();
     }
 
     @SneakyThrows
     @Test
     @Order(6)
-    void runningMultipleThreadsWithOneTask()  {
-        HelloThreads.runningMultipleThreadsWithOneTask(new Thread(() ->
+    void runInMultipleThreads() {
+        HelloThreads.runInMultipleThreads(new Thread(() ->
                 concurrentLinkedQueue.add(ThreadLocalRandom.current().nextInt(100))));
         assertEquals(3, concurrentLinkedQueue.size());
     }
 
-    private void checkContentQueue() {
+    private void verifyContentQueue() {
         assertEquals(1, concurrentLinkedQueue.size());
         assertEquals(5, concurrentLinkedQueue.element().intValue());
     }
