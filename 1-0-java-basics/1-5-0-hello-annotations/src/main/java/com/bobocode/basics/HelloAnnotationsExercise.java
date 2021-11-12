4bb6d0f8d0b1e7892a87b82364c0ef4bf2e9bc03
Taras Boychuk <tarass.boychuk@gmail.com>
2021-11-12 14:39:18 +0200

    Update hello-exercise * rename test, so it corresponds to the class name * update javadoc to make the task clearer

--- 1-0-java-basics/1-5-0-hello-annotations/src/main/java/com/bobocode/basics/HelloAnnotationsExercise.java
+++ 1-0-java-basics/1-5-0-hello-annotations/src/main/java/com/bobocode/basics/HelloAnnotationsExercise.java
@@ -1,11 +1,13 @@
 package com.bobocode.basics;
 
 /**
- * {@link HelloAnnotationsExercise} is an exercise class that should be annotation with appropriate @{@link Exercise}
- * annotation. With that annotation it should specify exercise name "hello-annotation-basic" and the default complexity
- * level.
+ * {@link HelloAnnotationsExercise} is an exercise class that is marked with be corresponding @{@link Exercise}
+ * annotation. The annotation value specifies exercise name "hello-annotation-basic". It does not specify any custom
+ * complexity level, because this exercise is a basic, which correspond to the default value provided by annotation.
  * <p>
  * todo: Create an annotation @{@link Exercise}.
+ * todo: Set its retention policy so it is visible at runtime
+ * todo: Set its target so it can only be applied to a class
  * todo: Add String value that will store exercise name
  * todo: Add complexityLevel with a default {@link Level} basic
  *
