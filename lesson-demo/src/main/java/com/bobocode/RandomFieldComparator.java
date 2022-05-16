1c9a73f67c94f316ba65bb89cd4dec6a4b71e9bb
Taras Boychuk <tarass.boychuk@gmail.com>
2022-05-16 15:37:24 +0300

    JU22 RandomFieldComparator * add compareOnlyAccessibleFields property

--- lesson-demo/src/main/java/com/bobocode/RandomFieldComparator.java
+++ lesson-demo/src/main/java/com/bobocode/RandomFieldComparator.java
@@ -6,23 +6,31 @@ import java.util.Comparator;
  * A generic comparator that is comparing a random field of the given class. The field is either primitive or
  * {@link Comparable}. It is chosen during comparator instance creation and is used for all comparisons.
  * <p>
- * If no field is available to compare, the constructor throws {@link IllegalArgumentException}
+ * By default it compares only accessible fields, but this can be configured via a constructor property. If no field is
+ * available to compare, the constructor throws {@link IllegalArgumentException}
  *
  * @param <T> the type of the objects that may be compared by this comparator
  */
 public class RandomFieldComparator<T> implements Comparator<T> {
 
     public RandomFieldComparator(Class<T> targetType) {
+        this(targetType, true);
+    }
+
+    /**
+     * A constructor that accepts a class and a property indicating which fields can be used for comparison. If property
+     * value is true, then only public fields or fields with public getters can be used.
+     *
+     * @param targetType                  a type of objects that may be compared
+     * @param compareOnlyAccessibleFields config property indicating if only publicly accessible fields can be used
+     */
+    public RandomFieldComparator(Class<T> targetType, boolean compareOnlyAccessibleFields) {
         throw new UnsupportedOperationException("This method should be implemented"); // todo:
     }
 
     /**
      * Compares two objects of the class T by the value of the field that was randomly chosen. It allows null values
-     * for the fields, and it treats null value grater than a non-null value.
-     *
-     * @param o1
-     * @param o2
-     * @return
+     * for the fields, and it treats null value grater than a non-null value (nulls last).
      */
     @Override
     public int compare(T o1, T o2) {
