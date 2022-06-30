276330765626b3ac97f5bb3efea2dcdfe2afdccf
Taras Boychuk <tboychuk@bobocode.com>
2022-06-30 18:36:11 +0300

    GP-146 - complete Heterogeneous Max Holder Exercise * implement method put * overload method put * implement method getMax

--- 1-0-java-basics/1-3-2-heterogeneous-max-holder/src/main/java/com/bobocode/basics/HeterogeneousMaxHolder.java
+++ 1-0-java-basics/1-3-2-heterogeneous-max-holder/src/main/java/com/bobocode/basics/HeterogeneousMaxHolder.java
@@ -1,6 +1,10 @@
 package com.bobocode.basics;
 
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Map;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * {@link HeterogeneousMaxHolder} is a multi-type container that holds maximum values per each type. It's kind of a
@@ -9,9 +13,10 @@ import java.util.Map;
  * It's based on the {@link Map} and provides an API that allows to put a value by type, and get a max value by type.
  */
 public class HeterogeneousMaxHolder {
+    private Map<Class<?>, Object> typeToMaxValueMap = new HashMap<>();
 
     /**
-     * A method put stores a provided value by its type, if the value is greater than the current maximum. In other words, the logic
+     * Stores a provided value by its type, if the value is greater than the current maximum. In other words, the logic
      * of this method makes sure that only max value is stored and everything else is ignored.
      * <p>
      * If the current max value is less than a provided one, or if it's null, then a provided value gets stored and the old
@@ -24,13 +29,13 @@ public class HeterogeneousMaxHolder {
      * @param <T>   value type parameter
      * @return a smaller value among the provided value and the current maximum
      */
-    // todo: implement a method according to javadoc
+    public <T extends Comparable<? super T>> T put(Class<T> key, T value) {
+        return put(key, value, Comparator.naturalOrder());
+    }
 
     /**
-     * An overloaded method put implements the same logic using a custom comparator. A given comparator is wrapped with 
-     * a null-safe comparator, considering null smaller than any non-null object. 
-     *
-     * All arguments must not be null.
+     * Implements a put logic using a custom comparator that is wrapped with a null-safe comparator,
+     * considering null smaller than any non-null object. See put method above. All arguments must not be null.
      *
      * @param key        a provided value type
      * @param value      a value to put
@@ -38,14 +43,27 @@ public class HeterogeneousMaxHolder {
      * @param <T>        value type parameter
      * @return a smaller value among the provided value and the current maximum
      */
-    // todo: implement a method according to javadoc
+    public <T> T put(Class<T> key, T value, Comparator<T> comparator) {
+        var currentMax = getMax(requireNonNull(key));
+        var nullSafeComparator = Comparator.nullsFirst(requireNonNull(comparator));
+        requireNonNull(value);
+        if (nullSafeComparator.compare(value, currentMax) > 0) {
+            typeToMaxValueMap.put(key, value);
+            return currentMax;
+        }
+        return value;
+    }
 
     /**
-     * A method getMax returns a max value by the given type. If no value is stored by this type, then it returns null.
+     * Returns a max value by the given type. If no value is stored by this type, then it returns null.
      *
      * @param key a provided value type
      * @param <T> value type parameter
      * @return current max value or null
      */
-    // todo: implement a method according to javadoc
+    public <T> T getMax(Class<T> key) {
+        requireNonNull(key);
+        var currentMax = typeToMaxValueMap.get(key);
+        return key.cast(currentMax);
+    }
 }
