94e0b7b19a792f7ddf6675b247812f77ac470517
Taras Boychuk <tboychuk@bobocode.com>
2022-07-16 11:11:13 +0300

    Complete custom Collector

--- lesson-demo/src/main/java/com/bobocode/CustomCollectorDemoApp.java
+++ lesson-demo/src/main/java/com/bobocode/CustomCollectorDemoApp.java
@@ -5,6 +5,7 @@ import com.bobocode.model.Account;
 import lombok.SneakyThrows;
 
 import java.util.Stack;
+import java.util.stream.Collector;
 
 public class CustomCollectorDemoApp {
     @SneakyThrows
@@ -13,8 +14,20 @@ public class CustomCollectorDemoApp {
                 .stream()
                 .map(Account::getEmail)
                 .collect(toStack());
-        
-        // todo: implement method toStack that returns a custom Collector 
+
+        while (!emailStack.isEmpty()) {
+            System.out.println(emailStack.pop());
+        } 
+    }
+
+    static <T> Collector<T, ?, Stack<T>> toStack() {
+        return Collector.of(
+                Stack::new,
+                Stack::push,
+                (stack1, stack2) -> {
+                    stack2.forEach(stack1::push);
+                    return stack1;
+                });
     }
 
 
