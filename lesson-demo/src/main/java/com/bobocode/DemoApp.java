1205a6a4a0acca938926124ea4082b1d278b40bf
Taras Boychuk <tboychuk@bobocode.com>
2022-11-29 08:08:59 +0200

    Implement comparing task

--- lesson-demo/src/main/java/com/bobocode/DemoApp.java
+++ lesson-demo/src/main/java/com/bobocode/DemoApp.java
@@ -1,6 +1,22 @@
 package com.bobocode;
 
+import com.bobocode.data.Accounts;
+import com.bobocode.model.Account;
+
+import java.util.Comparator;
+import java.util.stream.Stream;
+
 public class DemoApp {
     public static void main(String[] args) {
+        Accounts.generateAccountList(10)
+                .stream()
+                .max(comparing(Account::getBalance))
+                .ifPresent(System.out::println);
     }
+    
+    private static Comparator comparing() {
+        throw new UnsupportedOperationException();//todo:
+    }
+  
+    
 }
