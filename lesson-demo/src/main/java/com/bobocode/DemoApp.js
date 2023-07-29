8366350dc70360030073255c205d2f497f7b6517
Taras Boychuk <tboychuk@bobocode.com>
2023-07-29 12:01:29 +0300

    LinkedList in JS (completed)

--- lesson-demo/src/main/java/com/bobocode/DemoApp.js
+++ lesson-demo/src/main/java/com/bobocode/DemoApp.js
@@ -1,8 +1,4 @@
-
-
-
-
-let head = createLinkedList([1,2,3,4,5])
+let head = createLinkedList([1, 2, 3, 4, 5])
 printLinkedList(head)
 console.log("----------------");
 printReversedRecursively(head); // should print "5 -> 4 -> 3 -> 2 -> 1"
@@ -12,17 +8,53 @@ let newHead = reverseLinkedList(head);
 printLinkedList(newHead);// should print "5 -> 4 -> 3 -> 2 -> 1"
 
 function createLinkedList(arr) {
-    // todo:
+    let head = createNode(arr[0])
+    let current = head
+    for (let i = 1; i < arr.length; i++) {
+        current.next = createNode(arr[i])
+        current = current.next
+    }
+    return head
+}
+
+function createNode(value) {
+    return {
+        element: value,
+        next: null
+    }
 }
 
 function printLinkedList(head) {
-    // todo:
+    let current = head
+    let str = ""
+    while (current.next != null) {
+        str += current.element + " -> "
+        current = current.next;
+    }
+    str += current.element
+    console.log(str)
 }
 
 function printReversedRecursively(head) {
-    // todo:
+printReversedRecursivelyHelper(head)
+}
+
+function printReversedRecursivelyHelper(head) {
+    if (head != null) {
+        printReversedRecursivelyHelper(head.next)
+        console.log(head.element)
+    }
 }
 
 function reverseLinkedList(head) {
-    // todo:
+    let prev = head
+    let current = head.next
+    head.next = null
+    while (current != null) {
+        let next = current.next
+        current.next = prev
+        prev = current
+        current = next
+    }
+    return prev
 }
