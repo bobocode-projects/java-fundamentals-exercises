15123f19aa9ac16fc5f515ed85602126660ede20
Taras Boychuk <tboychuk@bobocode.com>
2023-08-28 08:17:47 +0300

    A simple Binary Search Tree

--- lesson-demo/src/main/java/com/bobocode/DemoApp.java
+++ lesson-demo/src/main/java/com/bobocode/DemoApp.java
@@ -2,46 +2,38 @@ package com.bobocode;
 
 import com.bobocode.util.ExerciseNotCompletedException;
 
+import java.util.function.Consumer;
+
 public class DemoApp {
     public static void main(String[] args) {
-        var intListHead = createLinkedList(1, 2, 3, 4, 5);
-        printLinkedList(intListHead);
+        var treeRoot = createBinarySearchTree(6, 4, 5, 2, 3, 7, 1);
+        inOrderTraversal(treeRoot, System.out::println); // should print 1,2,3,4,5,6,7
     }
 
     /**
-     * Creates a linked list based on the provided elements and returns a list head.
+     * Creates a binary search tree based on the given elements and returns the root node.
      *
      * @param elements
      * @param <T>
-     * @return created list head
+     * @return
      */
-    public static <T> Node<T> createLinkedList(T... elements) {
-        throw new ExerciseNotCompletedException();// todo:
+    public static <T extends Comparable<? super T>> Node<T> createBinarySearchTree(T... elements) {
+        throw new ExerciseNotCompletedException(); //todo:
     }
 
-    /**
-     * Prints a provided linked list in the following format:
-     * 1 -> 2 -> 3 -> 4 -> 5
-     *
-     * @param head a list head
-     */
-    public static void printLinkedList(Node<?> head) {
-        throw new ExerciseNotCompletedException();// todo:
+    public static <T> void inOrderTraversal(Node<T> root, Consumer<? super T> consumer) {
+        if (root != null) {
+            inOrderTraversal(root.left, consumer);
+            consumer.accept(root.element);
+            inOrderTraversal(root.right, consumer);
+        }
     }
 
-    /**
-     * Prints a provided linked list recursively in the following format:
-     * 1 -> 2 -> 3 -> 4 -> 5
-     *
-     * @param head a list head
-     */
-    public static void printLinkedListRecursively(Node<?> head) {
-        throw new ExerciseNotCompletedException();// todo:
-    }
 
     static class Node<T> {
         T element;
-        Node<T> next;
+        Node<T> left;
+        Node<T> right;
 
         public Node(T element) {
             this.element = element;
