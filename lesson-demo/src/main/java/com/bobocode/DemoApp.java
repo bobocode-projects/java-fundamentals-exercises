e71c88770a27593fbc8b66790380d39167ef0e0a
Taras Boychuk <tboychuk@bobocode.com>
2024-05-15 21:00:25 +0300

    Implement a custom method "comparing"

--- lesson-demo/src/main/java/com/bobocode/DemoApp.java
+++ lesson-demo/src/main/java/com/bobocode/DemoApp.java
@@ -1,6 +1,17 @@
 package com.bobocode;
 
+import com.bobocode.data.Accounts;
+import com.bobocode.model.Account;
+
+import static java.util.Comparator.comparing;// todo: comment this line 
+
 public class DemoApp {
     public static void main(String[] args) {
+        Accounts.generateAccountList(10)
+                .stream()
+                .max(comparing(Account::getBalance))
+                .ifPresent(System.out::println);
     }
+
+    // todo: implement a custom method "comparing"
 }
